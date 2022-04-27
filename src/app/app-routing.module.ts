c3dd5f749a2e48a7a6b7a6adc30a71b5c6b9c339
Agustin Martinez <65874500+chinomartinez@users.noreply.github.com>
2022-04-27 09:17:23 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", 
+    loadChildren :() => import("./ingreso-visitantes/ingreso-visitantes.module").then(m => m.IngresoVisitantesModule)
+  },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
